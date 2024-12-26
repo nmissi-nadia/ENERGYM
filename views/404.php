81f9dec82fa8e02657e38d4bf8dca9720da6deac
nmissi-nadia <nmissinadia@gmail.com>
2024-12-26 10:41:25 +0100

    Création des Classes et frontend pages authentification

---
+++ views/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>404</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="bg-indigo-900 relative overflow-hidden h-screen">
+     <img src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9" class="absolute h-full w-full object-cover"/>
+    <div class="inset-0 bg-black opacity-25 absolute">
+    </div>
+    <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+        <div class="w-full font-mono flex flex-col items-center relative z-10">
+            <h1 class="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+            Tu es tout seul ici
+          </h1>
+<button class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-100 h-auto" onclick="window.location.href='index.php'" type="submit">Retourne Page d'accueil</button>
+
+            <p class="font-extrabold text-8xl my-44 text-white animate-bounce">
+                404
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
