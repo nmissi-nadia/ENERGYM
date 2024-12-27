74c6c7793bf787e5e3da2e07b2bccad8e6223a67
FadwaElouah <fadwaelouah@gmail.com>
2024-12-27 22:57:50 +0100

    change le designe

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -40,11 +40,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 
 <header class="bg-purple-500 w-[100%]">
-<nav class="bg-purple-100 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+<nav class="bg-purple-100 border-gray-200 px-4 lg:px-6 py-2.5 ">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <img src="../../assets/images/energym.png" class="h-8 me-3 scale-[2.5]" alt="energym Logo" />
+            <img src="../../assets/images/energym.png" class="h-8 me-3 scale-[2.5] pl-[2rem]" alt="energym Logo" />
                 <div class="flex items-center lg:order-2">
-                    <a href="../logout.php" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Déconnexion</a>
+                    <a href="../logout.php" class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 text-gray-700 ">Déconnexion</a>
                     <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -54,13 +54,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700  rounded bg-primary-700 lg:bg-transparent lg:hover:bg-transparen lg:text-primary-700 lg:p-0 dark:text-white " aria-current="page">Home</a>
+                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700  rounded bg-primary-700 lg:bg-transparent lg:hover:bg-transparen lg:text-primary-700 lg:p-0 " aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Activités</a>
+                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0   x²    ">Activités</a>
                         </li>
                         <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Réservations</a>
+                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0   ">Réservations</a>
                         </li>
                        
                     </ul>
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php endif; ?>
 
     <!-- Membres inscrits -->
-    <h2>Membres Inscrits</h2>
+    <h2 class="titre">Membres Inscrits</h2>
     <table>
         <thead>
             <tr>
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </table>
 
     <!-- Réservations des membres -->
-    <h2>Réservations des Membres</h2>
+    <h2 class="titre">Réservations des Membres</h2>
     <table>
         <thead>
             <tr>
@@ -138,7 +138,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </table>
 
     <!-- Gestion des activités -->
-    <h2>Liste des Activités</h2>
+    <h2 class="titre">Liste des Activités</h2>
     <table>
         <thead>
             <tr>
@@ -165,7 +165,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </tbody>
     </table>
 
-    <h2>Gestion des Activités</h2>
+    <h2 class="titre">Gestion des Activités</h2>
 
     <h3 class="ajouter">Ajouter une activité</h3>
     <form method="POST" class="titreAjouter">
